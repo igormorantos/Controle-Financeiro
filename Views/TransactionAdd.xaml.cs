ce703bae301dc54e79d6c77149a2933853aa399e
igor <igor@DESKTOP-H1HTBV1>
2024-04-05 15:08:50 -0300

    Building views of application

--- Views/TransactionAdd.xaml.cs
+++ Views/TransactionAdd.xaml.cs
@@ -1,9 +1,80 @@
+using AppControleFinanceiro.Models;
+using AppControleFinanceiro.Repositories;
+using System.Text;
+
 namespace AppControleFinanceiro.Views;
 
 public partial class TransactionAdd : ContentPage
 {
-	public TransactionAdd()
+    private readonly ITransactionRepository _respository;
+	public TransactionAdd(ITransactionRepository respository)
 	{
 		InitializeComponent();
-	}
+        _respository = respository;
+    }
+
+    private void TapGestuRecognizer_Tapped(object sender, TappedEventArgs e)
+    {
+		Navigation.PopModalAsync();
+    }
+
+    private void OnButtonClicked_Save(object sender, EventArgs e)
+    {
+        if (IsValidData() == false) return;
+        
+        SaveTransactionInDataBase();
+        
+        //TODO - FECHAR TELA
+        Navigation.PopModalAsync();
+
+        var count = _respository.GetAll().Count;
+        
+        App.Current.MainPage.DisplayAlert("Mensagem!", $"Existem {count} registros no banco", "OK");
+    }
+
+    private void SaveTransactionInDataBase()
+    {
+
+        Transaction transactionm = new Transaction()
+        {
+            Name = EntryName.Text,
+            Type = RadioInCome.IsChecked ? TransactionType.Income : TransactionType.Expense,
+            Date = DatePicker.Date,
+            Value = double.Parse(EntryValue.Text)
+        };
+
+        _respository.Add(transactionm);
+    }
+
+    private bool IsValidData()
+    {
+        bool valid = true;
+        StringBuilder sb = new StringBuilder();
+
+        if (string.IsNullOrEmpty(EntryName.Text) || string.IsNullOrWhiteSpace(EntryName.Text))
+        {
+            sb.AppendLine("o campo 'Nome' deve ser preenchido");
+             valid = false;
+        }
+        
+        if (string.IsNullOrEmpty(EntryValue.Text) || string.IsNullOrWhiteSpace(EntryValue.Text))
+        {
+            sb.AppendLine("o campo 'valor' deve ser preenchido");
+            valid = false;
+        }
+
+        double result;
+        if(!string.IsNullOrEmpty(EntryValue.Text) && !double.TryParse(EntryValue.Text, out result))
+        {
+            sb.AppendLine("o campo 'Valor' é invalido");
+            valid = false;
+        }
+        if(valid == false)
+        {
+            LabelError.IsVisible = true;
+            LabelError.Text = sb.ToString();
+        }
+
+        return valid;
+    }
 }
--- Views/TransactionList.xaml.cs
+++ Views/TransactionList.xaml.cs
@@ -2,19 +2,24 @@ namespace AppControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
-	public TransactionList()
+    private TransactionAdd _transactictionAdd;
+	private TransactionEdit _transactionEdit;
+	
+	public TransactionList(TransactionAdd transactictionAdd, TransactionEdit transactictionEdit)
 	{
+        this._transactictionAdd = transactictionAdd;
+		this._transactionEdit = transactictionEdit; ;
 		InitializeComponent();
 	}
 
 	private void OnButtonClickToTransactionAdd(object sender, EventArgs args)
 	{
-		App.Current.MainPage = new TransactionAdd();
+		Navigation.PushModalAsync(_transactictionAdd);
 
     }
 
     private void OnButtonClickToTransactionEdit(object sender, EventArgs e)
     {
-        App.Current.MainPage = new TransactionEdit();
+        Navigation.PushModalAsync(_transactionEdit);
     }
 }
